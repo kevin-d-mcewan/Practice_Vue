dbde0eaf3230fd7e6066d10eab43403cb23508aa
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-11-17 22:04:25 -0800

    Added on-hover-text effect to change the title

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const Timer = {
 			timer: 0,
 		};
 	},
+	/* The `mounted()` lifecycle hook in Vue.js is called after the Vue instance has been mounted to the
+  DOM. In this code snippet, the `mounted()` method is setting up a timer using `setInterval()`. */
 	mounted() {
 		setInterval(() => {
 			this.timer++;
@@ -12,3 +14,13 @@ const Timer = {
 };
 
 Vue.createApp(Timer).mount("#timer");
+
+const ReactiveBehavior = {
+	data() {
+		return {
+			onHoverText: "Reactive message!",
+		};
+	},
+};
+
+Vue.createApp(ReactiveBehavior).mount("#to-hover-element");
