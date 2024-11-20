be655f6e346287efbcce3b0ef2a3f765cec8a070
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-11-19 19:44:30 -0800

    Created a List in Vue

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-const Timer = {
+const List = {
 	data() {
 		return {
-			timer: 0,
-		};
-	},
-	/* The `mounted()` lifecycle hook in Vue.js is called after the Vue instance has been mounted to the
-  DOM. In this code snippet, the `mounted()` method is setting up a timer using `setInterval()`. */
-	mounted() {
-		setInterval(() => {
-			this.timer++;
-		}, 1000);
-	},
-};
-
-Vue.createApp(Timer).mount("#timer");
-
-const ReactiveBehavior = {
-	data() {
-		return {
-			onHoverText: "Reactive message!",
+			listItems: [
+				{ item: "Study Vue Course" },
+				{ item: "Learn React Native" },
+				{ item: "Study Angular" },
+			],
 		};
 	},
 };
 
-Vue.createApp(ReactiveBehavior).mount("#to-hover-element");
+Vue.createApp(List).mount("#list");
