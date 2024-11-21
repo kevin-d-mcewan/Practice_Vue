6a3f84ce35c1d045866cdee127e90664db79e63b
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-11-20 22:29:59 -0800

    Added a 'select' option for the form

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ Vue.createApp({
 			// This will place whatever information 'inputText' recieves into <p id="fnInpTxt"></p>
 			inputText: "",
 			textAreaContent: "",
+			selected: "Search",
+			options: [
+				{
+					text: "From a search engine",
+					value: "Search",
+				},
+				{
+					text: "From a friend",
+					value: "Friend",
+				},
+				{
+					text: "From an add",
+					value: "Add",
+				},
+				{
+					text: "From Social Media",
+					value: "Social Media",
+				},
+				{
+					text: "Through dumb luck",
+					value: "luck",
+				},
+			],
 		};
 	},
 }).mount("#form");
